2e69274b12fc7c02bebfe75bf08ca660ee82b9ac
lethomas1975 <le.thomas@outlook.com>
2022-03-23 15:21:54 +0800

    blinking led template file

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED="Blinking_LED_PIC18F4550 code.c"
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED="${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1"
+POSSIBLE_DEPFILES="${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1.d"
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/Blinking_LED_PIC18F4550\ code.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=Blinking_LED_PIC18F4550 code.c
 
 
 CFLAGS=
@@ -81,7 +81,23 @@ MP_PROCESSOR_OPTION=18F4550
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/Blinking_LED_PIC18F4550\ code.p1: Blinking_LED_PIC18F4550\ code.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1".d 
+	@${RM} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1" 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o"${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1"  "Blinking_LED_PIC18F4550 code.c" 
+	@-${MV} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code".d "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1".d 
+	@${FIXDEPS} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1".d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/Blinking_LED_PIC18F4550\ code.p1: Blinking_LED_PIC18F4550\ code.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1".d 
+	@${RM} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1" 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o"${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1"  "Blinking_LED_PIC18F4550 code.c" 
+	@-${MV} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code".d "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1".d 
+	@${FIXDEPS} "${OBJECTDIR}/Blinking_LED_PIC18F4550 code.p1".d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
