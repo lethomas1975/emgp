655a98d0b62c31a0914feb54c1f62f110dd48faf
lethomas1975 <le.thomas@outlook.com>
2022-04-04 00:02:56 +0800

    main program to integrate everything

--- AutomatedCar.X/main.c
+++ AutomatedCar.X/main.c
@@ -10,10 +10,6 @@
 #include <xc.h>
 #include <pic18f4550.h>
 
-//#pragma config FOSC = INTOSC_HS  	// Internal oscillator, HS used by USB.
-#pragma config FOSC = INTOSCIO_EC   	// Internal oscillator, port function on RA6, EC used by USB. 
-#pragma config WDT = OFF            	// Disable watchdog timer
-
 #include "init.h"
 #include "common.h"
 #include "drive.h"
@@ -24,6 +20,7 @@ void main(void) {
     init();
     
     while (1) {
-        // drive
+        proximityDetection();
+        zebraDetected();
     }
 }
--- AutomatedCar.X/nbproject/Makefile-default.mk
+++ AutomatedCar.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c buzzer.c init.c drive.c zebra.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c buzzer.c init.c drive.c zebra.c common.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/buzzer.p1 ${OBJECTDIR}/init.p1 ${OBJECTDIR}/drive.p1 ${OBJECTDIR}/zebra.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/buzzer.p1.d ${OBJECTDIR}/init.p1.d ${OBJECTDIR}/drive.p1.d ${OBJECTDIR}/zebra.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/buzzer.p1 ${OBJECTDIR}/init.p1 ${OBJECTDIR}/drive.p1 ${OBJECTDIR}/zebra.p1 ${OBJECTDIR}/common.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/buzzer.p1.d ${OBJECTDIR}/init.p1.d ${OBJECTDIR}/drive.p1.d ${OBJECTDIR}/zebra.p1.d ${OBJECTDIR}/common.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/buzzer.p1 ${OBJECTDIR}/init.p1 ${OBJECTDIR}/drive.p1 ${OBJECTDIR}/zebra.p1
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/buzzer.p1 ${OBJECTDIR}/init.p1 ${OBJECTDIR}/drive.p1 ${OBJECTDIR}/zebra.p1 ${OBJECTDIR}/common.p1
 
 # Source Files
-SOURCEFILES=main.c buzzer.c init.c drive.c zebra.c
+SOURCEFILES=main.c buzzer.c init.c drive.c zebra.c common.c
 
 
 CFLAGS=
@@ -121,6 +121,14 @@ ${OBJECTDIR}/zebra.p1: zebra.c  nbproject/Makefile-${CND_CONF}.mk
 	@-${MV} ${OBJECTDIR}/zebra.d ${OBJECTDIR}/zebra.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/zebra.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/common.p1: common.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/common.p1.d 
+	@${RM} ${OBJECTDIR}/common.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/common.p1  common.c 
+	@-${MV} ${OBJECTDIR}/common.d ${OBJECTDIR}/common.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/common.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -161,6 +169,14 @@ ${OBJECTDIR}/zebra.p1: zebra.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/zebra.p1  zebra.c 
 	@-${MV} ${OBJECTDIR}/zebra.d ${OBJECTDIR}/zebra.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/zebra.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/common.p1: common.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/common.p1.d 
+	@${RM} ${OBJECTDIR}/common.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/common.p1  common.c 
+	@-${MV} ${OBJECTDIR}/common.d ${OBJECTDIR}/common.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/common.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
